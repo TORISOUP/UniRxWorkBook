3217611a460ec240b9a4b493cb4646c8fd8b31fb
TORISOUP <tori.birdstrike@gmail.com>
2015-09-29 02:42:02 +0900

    Throttle & ThrottleFirstを追加

---
+++ Assets/UniRxWorkBook/09_CombineLatest/Answer/Answer_Lesson_9_CombineLatest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UniRx;
+using UnityEngine.UI;
+
+namespace UniRxWorkBook
+{
+
+    public class Answer_Lesson_9_CombineLatest : MonoBehaviour
+    {
+        [SerializeField]
+        private InputField leftInput;
+        [SerializeField]
+        private InputField rightInput;
+        [SerializeField]
+        private Text resultLabel;
+        private void Start()
+        {
+            var leftStream = leftInput.OnValueChangeAsObservable().Select(x => Int32.Parse(x));
+            var rightStream = rightInput.OnValueChangeAsObservable().Select(x => Int32.Parse(x));
+
+            // ZipをCombineLatestに書き換えれば良い
+            // Zipは複数のストリームのメッセージが1つずつ揃った時に放出するものであったが、
+            // CombineLatestはすべてのストリームの値が揃ってなくても直前の値を代わりに使って放出するオペレータである
+            // [参考] http://reactivex.io/documentation/operators/combinelatest.html
+            leftStream
+                .CombineLatest(rightStream, (left, right) => left + right)
+                .SubscribeToText(resultLabel);
+        }
+    }
+}
---
+++ Assets/UniRxWorkBook/10_Trottle/Answer/Answer_Lesson_10_Throttle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UniRx;
+using UnityEngine.UI;
+
+namespace UniRxWorkBook
+{
+
+    public class Answer_Lesson_10_Throttle : MonoBehaviour
+    {
+        [SerializeField]
+        private InputField inputField;
+        [SerializeField]
+        private Text resultText;
+
+        private void Start()
+        {
+
+            // Throttleはメッセージが連続した時に、落ち着くまで待つオペレータ
+            //
+            // 指定した時間より短い間隔でメッセージが大量に来た場合、メッセージをすべて無視する
+            // そして指定時間以上経過したときに、受け取ったメッセージのうち一番最後に来ていたものを１つだけ放出する
+            // 今回のケースの様に、確定するまで値がブレたりするものに使うことで「安定するまで待つ」といった処理が簡単に書ける
+            //
+            // なお、フレーム数を指定できるThrottleFrameもある
+            inputField
+                .OnValueChangeAsObservable()
+                .Throttle(TimeSpan.FromSeconds(1))
+                .SubscribeToText(resultText);
+        }
+    }
+}
